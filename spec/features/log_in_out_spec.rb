dcf49efbf6ca9ed6d96033785787a0935e7179d8
Jesse Shawl <jesse@jesse.sh>
2021-12-11 10:24:36 -0600

    fix tests

--- spec/features/log_in_out_spec.rb
+++ spec/features/log_in_out_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'log in/out', type: :feature do
     it 'shows a log in button' do
       visit root_path
       expect(page).to have_button('Log In with Spotify')
-      find('[value="Log In with Spotify"]').click
+      find('[value="Log In with Spotify"]', match: :first).click
       expect(page.current_url).to match("spotify.com")
     end
   end
